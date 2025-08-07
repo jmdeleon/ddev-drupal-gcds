3e61c8786c9d97557488672dd7e8fa5e47a6d4bc
jmdeleon <jmdeleon@users.noreply.github.com>
2025-08-07 11:02:10 +0000

    fix:group composer and site install tasks

--- install-drupal-gcds.sh
+++ install-drupal-gcds.sh
@@ -3,11 +3,11 @@ mkdir drupal && cd drupal
 ddev config --project-type=drupal11 --docroot=web
 ddev start
 ddev composer create-project drupal/cms
-ddev composer config minimum-stability dev
 ddev composer require --dev drush/drush
-ddev drush site:install --account-name=admin --account-pass=pass -y
 ddev composer require drupal/bilingual_enfr_recipe
+ddev composer config minimum-stability dev
 ddev composer require drupal/gcds
+ddev drush site:install --account-name=admin --account-pass=pass -y
 ddev drush en -y twig_tweak twig_tools
 ddev drush recipe /var/www/html/recipes/bilingual_enfr_recipe
 ddev drush locale:check
