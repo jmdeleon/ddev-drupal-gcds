8ca43576a68fb197ea686f578126492b651e6673
jmdeleon <jmdeleon@users.noreply.github.com>
2025-12-23 10:36:06 +0000

    Update install-drupal-gcds.sh

--- install-drupal-gcds.sh
+++ install-drupal-gcds.sh
@@ -4,11 +4,13 @@ ddev config --project-type=drupal11 --docroot=web
 ddev start
 ddev composer create-project drupal/cms
 ddev composer config minimum-stability dev
+ddev composer install
+ddev composer drupal:recipe-unpack
 ddev composer require --dev drush/drush
-ddev composer require drupal/bilingual_enfr_recipe
-ddev composer require drupal/gcds
 ddev drush site:install --account-name=admin --account-pass=pass -y
+ddev composer require drupal/gcds
 ddev drush en -y twig_tweak twig_tools
+ddev composer require drupal/bilingual_enfr_recipe
 ddev drush recipe /var/www/html/recipes/bilingual_enfr_recipe
 ddev drush locale:check
 ddev drush locale:update
